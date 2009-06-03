784e748cc226abd931576d65cff441facaae7c72
Bohumir Zamecnik <bohumir@zamecnik.org>
2009-06-03 17:24:42 +0000

    * DynamicTresholdFilter works

--- src/GimpSharpPlugin/HalftoneLaboratory.cs
+++ src/GimpSharpPlugin/HalftoneLaboratory.cs
@@ -31,19 +31,32 @@ namespace Gimp.HalftoneLab
         override protected void Render(Drawable drawable) {
             Halftone.Image image = new GSImage(drawable);
 
-            TresholdFilter tresholdFilter = new MatrixTresholdFilter(
-                new TresholdMatrix(new int[1, 1] { { 128 } }));
+            DynamicTresholdFilter tresholdFilter = new DynamicTresholdFilter();
+            tresholdFilter.NoiseEnabled = true;
+            tresholdFilter.addTresholdRecord(0, TresholdMatrix.Generator.sampleMatrix, 0.0);
+            tresholdFilter.addTresholdRecord(32, TresholdMatrix.Generator.sampleMatrix, 0.125);
+            tresholdFilter.addTresholdRecord(64, TresholdMatrix.Generator.sampleMatrix, 0.25);
+            tresholdFilter.addTresholdRecord(96, TresholdMatrix.Generator.sampleMatrix, 0.375);
+            tresholdFilter.addTresholdRecord(128, TresholdMatrix.Generator.sampleMatrix, 0.5);
+            tresholdFilter.addTresholdRecord(160, TresholdMatrix.Generator.sampleMatrix, 0.625);
+            tresholdFilter.addTresholdRecord(192, TresholdMatrix.Generator.sampleMatrix, 0.75);
+            tresholdFilter.addTresholdRecord(224, TresholdMatrix.Generator.sampleMatrix, 0.875);
+            
+            //tresholdFilter.addTresholdRecord(100, TresholdMatrix.Generator.simpleTreshold);
+            //tresholdFilter.addTresholdRecord(150, TresholdMatrix.Generator.createBayerDispersedDotMatrix(3));
+            //TresholdFilter tresholdFilter = new MatrixTresholdFilter(
+            //    TresholdMatrix.Generator.simpleTreshold);
                 //TresholdMatrix.Generator.sampleMatrix);
             ScanningOrder scanOrder = new ScanlineScanningOrder();
-            //ErrorFilter errorFilter = null;
-            ErrorMatrix errorMatrix = new ErrorMatrix(
-                new double[2, 3] { { 0, 0, 7 }, { 3, 5, 1 } }, 1); // Floyd-Steinberg
-                //new double[2, 3] { { 0, 0, 1 }, {1, 1, 1} }, 1);
-            ErrorFilter errorFilter = //new PerturbedErrorFilter(
-                new RandomizedMatrixErrorFilter(
-                    errorMatrix,
-                    ErrorBuffer.createFromScanningOrder(
-                    scanOrder, errorMatrix.Height, image.Width));
+            ErrorFilter errorFilter = null;
+            //ErrorMatrix errorMatrix = new ErrorMatrix(
+            //    new double[2, 3] { { 0, 0, 7 }, { 3, 5, 1 } }, 1); // Floyd-Steinberg
+            ////new double[2, 3] { { 0, 0, 1 }, {1, 1, 1} }, 1);
+            //ErrorFilter errorFilter = //new PerturbedErrorFilter(
+            //    new MatrixErrorFilter(
+            //        errorMatrix,
+            //        ErrorBuffer.createFromScanningOrder(
+            //        scanOrder, errorMatrix.Height, image.Width));
             TresholdDitherAlgorithm alg = new TresholdDitherAlgorithm(
                 tresholdFilter, errorFilter, scanOrder);
 
