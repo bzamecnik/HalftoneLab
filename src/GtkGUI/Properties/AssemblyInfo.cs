a53366bbf4fa18c16745981aba822ef51fa559f2
Bohumir Zamecnik <bohumir@zamecnik.org>
2009-07-30 19:58:24 +0000

    * code moving   * GUI stuff -> new project GtkGUI   * testing project removed   * halftone.sln -> HalftoneLab.sln

--- src/GtkGUI/Properties/AssemblyInfo.cs
+++ src/GtkGUI/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("testing")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("HalftoneLabGtkGUI")]
+[assembly: AssemblyDescription("HalftoneLab GTK GUI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("testing")]
+[assembly: AssemblyCompany("Bohumir Zamecnik")]
+[assembly: AssemblyProduct("Halftone Laboratory")]
 [assembly: AssemblyCopyright("Copyright ©  2009")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Copyright © Bohumir Zamecnik, 2009")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5849c893-4e49-4da0-aaf9-f6ab7cbe620c")]
+[assembly: Guid("b5e33ddd-d231-4db3-94db-4a92f13ced38")]
 
 // Version information for an assembly consists of the following four values:
 //
